c69391c5af08e471f7a7a192836290eb19fb4880
Magesh <magesh@sedintechnologies.com>
2019-10-03 15:30:55 +0530

    created the basic files needed for react workshop

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello React,Webpack 4 & Babel 7! Changed</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
